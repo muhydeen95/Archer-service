02162ce8b98123217f9b6d57dca15ab4698abe56
ALABI MUHYDEEN OLANIYI <48595796+muhydeen95@users.noreply.github.com>
2020-07-11 06:40:12 +0100

    Update app.js
    Counter effect added

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+//Counter effect
+
+document.addEventListener("DOMContentLoaded", () => {
+ function counter(id, start, end, duration) {
+  let obj = document.getElementById(id),
+   current = start,
+   range = end - start,
+   increment = end > start ? 1 : -1,
+   step = Math.abs(Math.floor(duration / range)),
+   timer = setInterval(() => {
+    current += increment;
+    obj.textContent = current;
+    if (current == end) {
+     clearInterval(timer);
+    }
+   }, step);
+ }
+ counter("count1", 0, 5000, 3000);
+ counter("count2", 0, 300, 3000);
+ counter("count3", 0, 100, 3000);
+ counter("count4", 1000, 3000, 1000);
+});
